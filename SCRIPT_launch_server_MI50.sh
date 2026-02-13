768e92608341cba562332680172a697d0ec95854
iacopPBK <iacopPBK@dummy.com>
2026-02-13 23:29:04 +0100

    gfx906: readme update and, tested models in launch_server_MI50.sh, every model use qwen-code correctly

--- SCRIPT_launch_server_MI50.sh
+++ SCRIPT_launch_server_MI50.sh
@@ -26,7 +26,18 @@ export GGML_BACKEND_HIP=1
 export HCC_AMDGPU_TARGET=gfx906
 
 # Model path 
+MODEL_PATH="/media/iacoppbk/80F42C9BF42C96061/llms/cerebras_Qwen3-Coder-REAP-25B-A3B-Q8_0.gguf"
+MODEL_PATH="/media/iacoppbk/80F42C9BF42C96061/llms/Qwen3-VL-30B-A3B-Thinking-Q4_1.gguf"
+MODEL_PATH="/media/iacoppbk/80F42C9BF42C96061/llms/Qwen3-4B-Instruct-2507-Q4_1.gguf"
+MODEL_PATH="/media/iacoppbk/80F42C9BF42C96061/llms/openai_gpt-oss-20b-MXFP4.gguf"
+MODEL_PATH="/media/iacoppbk/80F42C9BF42C96061/llms/GLM-4.7-Flash-Q4_1.gguf"
+MODEL_PATH="/media/iacoppbk/80F42C9BF42C96061/llms/Qwen3-4B-Instruct-2507-Q8_0.gguf"
+MODEL_PATH="/media/iacoppbk/80F42C9BF42C96061/llms/Ministral-3-14B-Reasoning-2512-Q8_0.gguf"
 MODEL_PATH="/path/..."
+
+
+
+
 
 # Vision projector path (uncomment for multimodal models)
 #MMPROJ_PATH="/path/..."
@@ -50,18 +61,18 @@ MODEL_PATH="/path/..."
 
 
 ./build/bin/llama-server \
+    --spec-type ngram-mod \
+    --spec-ngram-size-n 24 \
+    --draft-min 48 \
+    --draft-max 64 \
     -m "$MODEL_PATH" \
-        #--spec-type ngram-mod \
-        #--spec-ngram-size-n 24 \
-        #--draft-min 48 \
-        #--draft-max 64 \
     -ngl 99 \
     -fa on \
-    -ctk f16 \
+    -ctk q8_0\
     -ctv f16 \
     --host 0.0.0.0 \
     --port 8080 \
-    -c 800 \
+    -c 80000 \
     -b 2048 \
     -ub 2048 \
     --jinja
